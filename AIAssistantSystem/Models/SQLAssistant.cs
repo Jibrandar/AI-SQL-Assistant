f77a8d4301c60a6c817fb30280dfb2b6cc771345
Jibrandar <jibrandar0@gmail.com>
2026-09-09 21:53:06 +0530

    Initital push

---
+++ AIAssistantSystem/Models/SQLAssistant.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.AI;
+using OpenAI;
+using System.ClientModel;
+
+namespace AIAssistantSystem.Models
+{
+    internal class SQLAssistant
+    {
+        private readonly string _apiKey = Environment.GetEnvironmentVariable("GROQ_API_KEY");
+        readonly ApiKeyCredential credential = null;
+        readonly OpenAIClient groqClient = null;
+        readonly OpenAIClientOptions options = null;
+        readonly IChatClient client = null;
+
+        public SQLAssistant()
+        {
+            credential = new ApiKeyCredential(_apiKey);
+            options = new OpenAIClientOptions()
+            {
+                Endpoint = new Uri("https://api.groq.com/openai/v1")
+            };
+
+            groqClient = new OpenAIClient(credential, options);
+            var chatClient = groqClient.GetChatClient("openai/gpt-oss-20b");
+            client = chatClient.AsIChatClient();
+        }
+
+        public async Task<string> GenerateSelectQuery(string requirement)
+        {
+            string systemPrompt = "You are an SQL expert.You will get an select query in english. convert the english sentence into a sql select query and explain the query to the user. If any other query given dont generate anything and return with proper response";
+            var response = await client.GetResponseAsync([
+                new(ChatRole.System,systemPrompt),
+               new(ChatRole.User,requirement)
+                ]);
+            return response.Text;
+        }
+
+
+        public async Task<string> GenerateInsertQuery(string requirement)
+        {
+            string systemPrompt = "You are an SQL expert.You will get an insert query in english. convert the english sentence into a sql insert query and explain the query to the user. If any other query given dont generate anything and return with proper response";
+            var response = await client.GetResponseAsync([
+                new(ChatRole.System,systemPrompt),
+                new(ChatRole.User,requirement)
+                ]);
+
+            return response.Text;
+        }
+
+        public async Task<string> GenerateDeleteQuery(string requirement)
+        {
+            string systemPrompt = "You are an SQL expert.You will get an Delete query in english. convert the english sentence into a sql Delete query and explain the query to the user. If any other query given dont generate anything and return with proper response";
+
+            var response = await client.GetResponseAsync([
+                new(ChatRole.System,systemPrompt),
+                new(ChatRole.User,requirement)
+                ]);
+
+            return response.Text;
+        }
+
+        public async Task<string> GenerateUpdateQuery(string requirement)
+        {
+            string systemPrompt = "You are an SQL expert.You will get an Update query in english. convert the english sentence into a sql update query and explain the query to the user. If any other query given dont generate anything and return with proper response";
+            var response = await client.GetResponseAsync([
+                new(ChatRole.System,systemPrompt),
+                new(ChatRole.User,requirement)
+                ]);
+            return response.Text;
+        }
+    }
+}
---
+++ AIAssistantSystem/Program.cs
@@ -0,0 +1,95 @@
+﻿using AIAssistantSystem.Models;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+       
+        SQLAssistant sqlAssistant = new SQLAssistant();
+
+        Console.WriteLine("==========================================================================================");
+        Console.WriteLine("AI SQL ASSISTANT");
+        Console.WriteLine("==========================================================================================");
+        while (true)
+        {
+            Console.WriteLine("==========================================================================================");
+
+            Console.WriteLine("1. Select Query");
+            Console.WriteLine("2. Insert Query");
+            Console.WriteLine("3. Update Query");
+            Console.WriteLine("4. Delete Query");
+            Console.WriteLine("5. Exit ");
+
+            Console.WriteLine("Enter Your Choice");
+            var choice = Console.ReadLine();
+
+            var answer = "";
+            switch (choice)
+            {
+
+                case "1":
+                    Console.WriteLine("SELECT query selected");
+                    Console.WriteLine("Describe the SELECT query you want");
+                    var selectQuery = Console.ReadLine();
+                    answer = await sqlAssistant.GenerateSelectQuery(selectQuery);
+                    Console.WriteLine(answer);
+                    break;
+                case "2":
+                    Console.WriteLine("INSERT query selected");
+
+                    Console.WriteLine("Describe the Insert query you want");
+                    var insertQuery = Console.ReadLine();
+                    answer = await sqlAssistant.GenerateInsertQuery(insertQuery);
+                    Console.WriteLine(answer);
+                    break;
+                case "3":
+                    Console.WriteLine("UPDATE query selected");
+
+
+                    Console.WriteLine("Describe the update query you want");
+                    var updateQuery = Console.ReadLine();
+                    answer = await sqlAssistant.GenerateUpdateQuery(updateQuery);
+                    Console.WriteLine(answer);
+                    break;
+
+
+                case "4":
+                    Console.WriteLine("DELETE query selected");
+                    Console.WriteLine("Delete operation can permanenetly delete data. Do you want to coninue (Yes OR No");
+
+                    var confirmation = Console.ReadLine();
+                    if (confirmation == "Yes")
+                    {
+                        Console.WriteLine("Describe the DELETE query you want");
+                        var deleteQuery = Console.ReadLine();
+                        answer = await sqlAssistant.GenerateDeleteQuery(deleteQuery);
+                        Console.WriteLine(answer);
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                case "5":
+                    Console.WriteLine("Exiting...........");
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice please select between 1-5");
+                    break;
+
+
+            }
+
+
+
+            if (choice == "5")
+            {
+                break;
+            }
+
+           
+        }
+
+
+    }
+}
